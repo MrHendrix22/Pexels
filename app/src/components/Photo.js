13ed89f86fd00b00cc268093b93308aa350c3c93
Hasani Hendrix <hasanihendrix@hasanis-mini.lan>
2022-10-15 12:20:46 -0400

    creating new components

---
+++ app/src/components/Photo.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+
+class Photo extends React.component {
+
+  constructor() {
+    super();
+  }
+
+  componentDidMount() {
+
+  }
+
+  render() {
+  
+    return (
+      <h1>Photo Component</h1>
+    );
+  }
+}
+
+export default Photo;
