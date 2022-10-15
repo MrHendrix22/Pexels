a9f8e8c3348c1291e4c920ed6b8b9b2aafc2bbdb
Hasani Hendrix <hasanihendrix@hasanis-mini.lan>
2022-10-15 12:46:50 -0400

    create photo component

--- app/src/App.js
+++ app/src/App.js
@@ -2,35 +2,14 @@ import logo from "./logo.svg";
 import React from "react";
 import "./App.css";
 import Photo from "./components/Photo";
+import {Container} from 'react-bootstrap'
 
-class App extends React.component {
-  constructor() {
-    super();
+function App() {
+  return (
+    <Container>
+      <Photo></Photo>
+    </Container>
+  );
   }
-
-  componentDidMount() {}
-
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          <Photo></Photo>
-        </header>
-      </div>
-    );
-  }
-}
 
 export default App;
--- app/src/components/Photo.js
+++ app/src/components/Photo.js
@@ -1,22 +1,22 @@
 import React from "react";
+import { Button, Card } from "react-bootstrap";
 
-
-class Photo extends React.component {
-
-  constructor() {
-    super();
-  }
-
-  componentDidMount() {
-
-  }
-
-  render() {
-  
-    return (
-      <h1>Photo Component</h1>
-    );
-  }
+function Photo() {
+  return (
+    <Card style={{ width: "18rem" }}>
+      <Card.Img
+        variant="top"
+        src="https://pbs.twimg.com/profile_images/443395572783800322/nXTuit5o_400x400.jpeg"
+      />
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          This is the storyline
+        </Card.Text>
+        <Button variant="primary">Check out</Button>
+      </Card.Body>
+    </Card>
+  );
 }
 
 export default Photo;
